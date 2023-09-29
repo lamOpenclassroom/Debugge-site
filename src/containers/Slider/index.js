5618c8519562acfb1cf66a0f011e8bccfdc8837c
lamtaoui mohamed <el-hafid-@hotmail.fr>
2023-09-29 20:35:37 +0200

    commit correction page blanche

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -12,9 +12,9 @@ const Slider = () => {
   );
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      () => setIndex(index < byDateDesc.length -1 ? index + 1 : 0),
       5000
-    );
+   );
   };
   useEffect(() => {
     nextCard();
